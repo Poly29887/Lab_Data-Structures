8fd1fcac931c11d8c6b8230cce10ad39ae837000
Poly29887 <tanapon1325@gmail.com>
2021-09-04 02:01:12 +0700

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,15 @@
-e = [2,1,3]
-for em in range(len(e)):
-  e[em]-=1
+def manangeStack(command,value = None):
+  if value == None:
+   print('no value')
+  else:
+   print(value)
+  
+manangeStack(2,3)
+manangeStack(2)
 
-print(e)
+list = [3,3,1,3,2]
+sum = 0
+for e in list:
+  if int(e) == 3:
+    list.remove(e)
+print(list)
