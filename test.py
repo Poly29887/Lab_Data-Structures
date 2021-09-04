fad9275c4710ebc543ae22a7d5205c32a264d4de
Poly29887 <tanapon1325@gmail.com>
2021-09-04 14:03:12 +0700

    Update test.py

--- test.py
+++ test.py
@@ -1,15 +1,7 @@
-def manangeStack(command,value = None):
-  if value == None:
-   print('no value')
-  else:
-   print(value)
-  
-manangeStack(2,3)
-manangeStack(2)
+inp = 'f'
+print(ord(inp))
+print(chr(110))
 
-list = [3,3,1,3,2]
-sum = 0
-for e in list:
-  if int(e) == 3:
-    list.remove(e)
-print(list)
+di= -2
+x = 3-di 
+print(x)
